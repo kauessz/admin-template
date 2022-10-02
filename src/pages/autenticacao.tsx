99ddd2d46882480ba297e39763c0154378f73be4
kauessz <ssz.kaue@gmail.com>
2022-10-02 17:49:11 -0300

    INTEGRAÇÃO FIREBASE

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,25 +1,48 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import { IconeAtencao } from "../components/icons";
 
 export default function Autenticacao() {
+    const [erro, setErro] = useState(null)
     const [modo, setModo] = useState<'login' | 'cadastro'>('login')
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
 
+    function exibirErro(msg, tempoEmSegundos = 5) {
+        setErro(msg)
+        setTimeout(() => setErro(null), tempoEmSegundos * 1000)
+    }
+
     function submit() {
         if(modo === 'login') {
             console.log('login')
+            exibirErro('Ocorreu um erro no login')
         }else {
             console.log('cadastro')
+            exibirErro('Ocorreu um erro no cadastro')
         }
     }
 
     return (
-        <div className={`flex flex-col h-screen items-center justify-center`}>
-            <div className={`w-1/2`}>
-                <h1 className={`text-xl font-bold mb-5`}>
+        <div className={`flex h-screen items-center justify-center`}>
+            <div className={`hidden md:block md:w-1/2 lg:w-2/3`}>
+                <img src="http://source.unsplash.com/random" alt="Imagem da tela de autenticação" 
+                    className={`h-screen w-full object-cover`}/>
+            </div>
+            <div className={`m-10 w-full md:w-1/2 lg:w-1/3`}>
+                <h1 className={`text-3xl font-bold mb-5`}>
                     {modo === 'login' ? 'Entre com a sua conta' : 'Cadastre-se'}
                 </h1>
+
+                {erro ? (
+                    <div className={`flex items-center bg-red-400 text-white py-3 px-5 my-2
+                        border border-red-700 rounded-lg`}>
+                        {IconeAtencao()}
+                        <span className="ml-3">{erro}</span>
+                    </div>
+                ) : false}
+                
+
                 <AuthInput
                     label="Email"
                     tipo="email"
@@ -45,6 +68,21 @@ export default function Autenticacao() {
                     text-white rounded-lg px-4 py-3`}>
                     Entrar com Google
                 </button>
+                {modo === 'login' ? (
+                    <p className="mt-8">
+                        Novo por aqui?
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}> Crie uma conta gratuitamente
+                        </a>
+                    </p>
+                ) : (
+                    <p className="mt-8">
+                        Já possui uma conta?
+                        <a onClick={() => setModo('login')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}> Entre com sua conta
+                        </a>
+                    </p>
+                )}
             </div>
         </div>
     )
